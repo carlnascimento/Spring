a9931ad33003e1b7cef8e4a5f964e8a89ea40642
Carlos Nascimento <carlosnascimento.ms@outlook.com>
2024-06-08 11:57:11 -0300

    inicial commit

---
+++ src/main/java/br/com/nascimentonetworks/segundo/SegundoApplication.java
@@ -0,0 +1,13 @@
+package br.com.nascimentonetworks.segundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nascimentonetworks/segundo/SegundoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nascimentonetworks.segundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
